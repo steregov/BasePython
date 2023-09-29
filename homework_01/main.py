f93ce01fef25d28fead0889498b1407d522ce75e
steregov <steregov@gmail.com>
2023-09-30 03:11:01 +0700

    WH1 final Edit1

--- homework_01/main.py
+++ homework_01/main.py
@@ -44,10 +44,10 @@ def filter_numbers(nums_list, flt_type):
         for num in nums_list:
             count_dif = 0
             for i in range(2, num-1):
-                if num % 2 == 0:
+                if num % i == 0:
                     count_dif = count_dif+1
             if count_dif == 0:
                 result.append(num)
         return result
 
-print(filter_numbers([12, 23, 30, 31, 34], PRIME))
+print(filter_numbers([913, 6887, 4569, 9279, 1559, 927, 263, 6405, 3413, 6353, 4569, 2151, 4321, 4789, 161, 5501, 5787, 1053, 5147, 6667, 8695, 8435, 4347, 6101, 8911, 2353, 1891, 1705, 6017, 3017, 3639, 3045, 7093, 5959, 9791, 5505, 1489, 655, 9305, 1145], PRIME))
