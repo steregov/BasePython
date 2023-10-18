454fc58a1906f30b7e40deea745d0baf72d916a2
steregov <steregov@gmail.com>
2023-10-18 22:35:15 +0700

    WH02 Edit7 (car)

--- homework_02/car.py
+++ homework_02/car.py
@@ -1,3 +1,13 @@
+
 """
 создайте класс `Car`, наследник `Vehicle`
 """
+from homework_02 import base
+
+class Car(base.Vehicle):
+    def __init__(self, weight, fuel, fuel_consumption, engine=None):
+        super().__init__(weight, fuel, fuel_consumption)
+        self.engine = engine
+
+    def set_engine(self, add_engine):
+        self.engine = add_engine
