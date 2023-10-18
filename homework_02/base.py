d13f5c022234daf34d874420535153f0f9da031d
steregov <steregov@gmail.com>
2023-10-18 21:32:51 +0700

    WH02 Edit3 (method start)

--- homework_02/base.py
+++ homework_02/base.py
@@ -10,9 +10,7 @@ class Vehicle(ABC):
     pass
 
     def start(self):
-        if self.started:
-            self.started = False
-        else:
+        if not self.started:
             if self.fuel > 0:
                 self.started = True
             else:
