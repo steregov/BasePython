04b1178c3a898c5d26bfa1ff77fab543ea4db7e6
steregov <steregov@gmail.com>
2023-10-18 22:45:48 +0700

    WH02 Edit8 (cargo)

--- homework_02/plane.py
+++ homework_02/plane.py
@@ -1,3 +1,19 @@
 """
 создайте класс `Plane`, наследник `Vehicle`
 """
+from homework_02 import base
+from homework_02 import exceptions
+
+class Plane(base.Vehicle):
+    def __init__(self, weight, fuel, fuel_consumption, cargo=None, max_cargo=None):
+        super().__init__(weight, fuel, fuel_consumption)
+        self.cargo = cargo
+        self.max_cargo = max_cargo
+
+    def load_cargo(self, val_cargo):
+        if self.cargo + val_cargo > self.max_cargo:
+            raise exceptions.CargoOverload
+        else:
+            self.cargo = self.cargo + val_cargo
+
+#cargo и max_cargo
