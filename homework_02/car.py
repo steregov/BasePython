138e29b43d4f615e2e947f2aec72f04834489610
steregov <steregov@gmail.com>
2023-10-25 17:12:31 +0700

    WH02 Edit12 (cargo)

--- homework_02/car.py
+++ homework_02/car.py
@@ -2,13 +2,15 @@
 """
 создайте класс `Car`, наследник `Vehicle`
 """
-from homework_02 import base, engine
+from homework_02 import base
+from homework_02.engine import Engine
+
 
 class Car(base.Vehicle):
     def __init__(self, weight, fuel, fuel_consumption, engine=None):
         super().__init__(weight, fuel, fuel_consumption)
         self.engine = engine
 
-    def set_engine(self, add_engine: object):
-        if isinstance(add_engine, engine):
+    def set_engine(self, add_engine):
+        if isinstance(add_engine, Engine):
             self.engine = add_engine
--- homework_02/engine.py
+++ homework_02/engine.py
@@ -4,7 +4,6 @@ create dataclass `Engine`
 """
 
 @dataclass()
-#(init=True) -- делать или не делать инит
 class Engine:
     volume: int
     pistons: int
