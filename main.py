3838b7beb7871e7d64e94789d30b7f65cdb9892a
steregov <steregov@gmail.com>
2023-11-24 01:52:10 +0700

    HW03 Docker Edit1

---
+++ main.py
@@ -0,0 +1,16 @@
+
+import datetime
+from fastapi import FastAPI, Query, Path
+
+app = FastAPI()
+
+@app.get("/ping/")
+def hw_ping():
+    return {"message": "pong"}
+
+@app.get("/login/{login}/{pwd}/")
+def test_address(login: str, pwd: str):
+    if login == 'user' and pwd == '123':
+        return {"message": f"Hello {login}!!!"}
+    else:
+        return {"message": f"Login or Password incorrect!"}
